c9cad3fb2a765214bbd8cce31b210e9f06de1397
darshdesai00 <darshdesai50@icloud.com>
2025-11-02 19:39:58 -0800

    working on milestone 2 and fixing errors

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <tuple>
 using namespace std;
 
 int main() {
     // declarations
-    map<string, vector<string>> villagerColors;
+    string, tuple<int, map<string, string>> villagers;
 
     // insert elements into the map
-    villagerColors["Audie"] = {"Orange", "Yellow", "Red"};
-    villagerColors["Raymond"] = {"Black", "Gray", "White"};
-    villagerColors.insert({"Marshal", {"Blue", "White", "Black"}});
+    villagers["Audie"] = make_tuple{"Orange", "Yellow", "Red"};
+    villagers["Raymond"] = make_tuple{"Black", "Gray", "White"};
+    villagers.({"Marshal", {"Blue", "White", "Black"}});
 
     // access the map using a range-based for loop
     cout << "Villagers and their favorite colors (range-based for loop):" << endl;
